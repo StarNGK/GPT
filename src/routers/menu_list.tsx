6c74337bf3f3a157b53e87b540995eab287d741e
StarCloud <115127996+StarNGK@users.noreply.github.com>
2023-06-22 00:52:28 +0800

    Update menu_list.tsx

--- src/routers/menu_list.tsx
+++ src/routers/menu_list.tsx
@@ -51,12 +51,6 @@ const web = [
     name: '邀请',
     icon: <UsergroupAddOutlined />,
     message: '账户余额和充值套餐记录'
-  },
-  {
-    path: 'https://github.com/79E/ChatGpt-Web',
-    name: '项目地址',
-    icon: <GithubOutlined />,
-    message: '免费开源可商业化AiWeb项目'
   }
 ]
 
