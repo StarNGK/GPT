50286a34563dabcd864c36ef732f11dd35c39951
StarCloud <115127996+StarNGK@users.noreply.github.com>
2023-11-29 13:01:15 +0800

    Delete src directory

--- src/store/async.ts
+++
@@ -1,8 +0,0 @@
-import chatAsync from './chat/async'
-import drawAsync from './draw/async'
-import userAsync from './user/async'
-import shopAsync from './shop/async'
-import configAsync from './config/async'
-import personaAsync from './persona/async'
-import pluginAsync from './plugin/async'
-export { chatAsync, drawAsync, userAsync, shopAsync, configAsync, personaAsync, pluginAsync }
--- src/utils/filterObjectNull.ts
+++
@@ -1,6 +0,0 @@
-export function filterObjectNull(obj: { [key: string]: any }): any {
-  const params = Object.keys(obj)
-    .filter((key) => obj[key] !== '' && obj[key] !== null && obj[key] !== undefined)
-    .reduce((acc, key) => ({ ...acc, [key]: obj[key] }), {})
-  return { ...params }
-}
